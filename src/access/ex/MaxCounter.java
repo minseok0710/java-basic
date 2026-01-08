462a911b89f7416bb26e73d8cda541cec0b82ce3
minseok <mikamg@naver.com>
2026-01-08 09:53:16 +0900

    docs: MaxCounter 파일에 프리픽스 개념정리

--- src/access/ex/MaxCounter.java
+++ src/access/ex/MaxCounter.java
@@ -33,3 +33,13 @@ public class MaxCounter {
     }
 
 }
+
+// 프리픽스 정리
+
+//태그,설명 (언제 쓰는가?),예시
+//feat,새로운 기능 추가,feat: 회원 가입 기능 구현
+//fix,버그 수정,fix: 로그인 시 NullPointerException 발생 해결
+//refactor,기능 변경 없이 코드 구조 개선,refactor: 중복된 입금 로직 제거 및 메서드 추출
+//docs,"문서 수정 (README, 주석 등)",docs: 클래스 설명 주석 추가
+//style,"코드 포맷팅 (오타, 띄어쓰기 등 로직 영향 X)",style: 줄바꿈 및 들여쓰기 수정
+//test,테스트 코드 추가/수정,test: 계좌 이체 테스트 코드 작성
